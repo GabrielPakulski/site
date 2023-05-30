71557a8d2ea53ea4b487a2a0efb544e59ca248c4
GabrielPakulski <gabrielpakulski@hotmail.com>
2023-05-29 21:00:37 -0300

    Create 404 html file

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
@@ -14,3 +16,7 @@ def contact(request):
         'email': 'gabrielpakulski@hotmail.com'
     }
     return render(request, 'contact.html')
+
+def error404(request, e):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
--- website/urls.py
+++ website/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'))
 ]
+
+handler404 = views.error404
